ed557cd0622a7ba5d236a5d86ce686345047d78f
Anurag Pal <anuragpalofficial@gmail.com>
2020-06-15 15:10:59 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,55 @@
+// document.onkeydown = function (e) {
+//         return false;
+// }
+
+
+
+function scrol(){
+  var x = document.getElementById("timcon");
+  x.scrollTop+=10
+}
+    setTimeout("scrol()", 10000);
+    setInterval ("scrol()", 500);
+
+function linux1(){
+  document.getElementById("ti").innerHTML = "Cosmogic - Linux Introduction";
+  document.getElementById("vid_frame").src="https://www.youtube.com/embed/V3yQbVVYKlY?autoplay=1&rel=0&controls=1";}
+
+  function linux2(){
+    document.getElementById("ti").innerHTML = "Cosmogic - Linux Shell";
+    document.getElementById("vid_frame").src="https://www.youtube.com/embed/MNBGGCXqguk?autoplay=1&rel=0&controls=1";
+  }
+  function linux3(){
+    document.getElementById("ti").innerHTML = "Cosmogic - File System";
+    document.getElementById("vid_frame").src="https://www.youtube.com/embed/OhOqSp11W6Y?autoplay=1&rel=0&controls=1";
+  }
+  function linux4(){
+    document.getElementById("ti").innerHTML = "Cosmogic - Navigation";
+    document.getElementById("vid_frame").src="https://www.youtube.com/embed/RDCu1DiX_h8?autoplay=1&rel=0&controls=1";
+  }
+  function linux5(){
+    document.getElementById("ti").innerHTML = "Cosmogic - File Manipulation";
+    document.getElementById("vid_frame").src="https://www.youtube.com/embed/gvc0y656JrQ?autoplay=1&rel=0&controls=1";
+  }
+function linux6(){
+  document.getElementById("ti").innerHTML = "Cosmogic - Redirection";
+  document.getElementById("vid_frame").src="https://www.youtube.com/embed/ANP2Q4jYX3c?autoplay=1&rel=0&controls=1";
+}
+
+
+
+
+
+// HTML links
+
+function html1(){
+  document.getElementById("ti").innerHTML = "Cosmogic - HTML Introduction Part 1 Tutorial";
+  document.getElementById("vid_frame").src="https://www.youtube.com/embed/lCbLb0L91oY?autoplay=1&rel=0&controls=1";}
+function html2(){
+  document.getElementById("ti").innerHTML = "Cosmogic - HTML Introduction Part 2 Tutorial";
+  document.getElementById("vid_frame").src="https://www.youtube.com/embed/B9jrTFlamcU?autoplay=1&rel=0&controls=1";
+}
+function html3(){
+  document.getElementById("ti").innerHTML = "Cosmogic - CSS (Cascading Stylesheet) Tutorial";
+  document.getElementById("vid_frame").src="https://www.youtube.com/embed/h6s9gV13N9w?autoplay=1&rel=0&controls=1";
+}
